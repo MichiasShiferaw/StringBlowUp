0faee83507a5b964249f9cf3262d186b3ee2b225
TheShiferawShow <68077955+TheShiferawShow@users.noreply.github.com>
2020-12-07 21:01:23 -0500

    Update BlowUp.java

--- BlowUp.java
+++ BlowUp.java
@@ -6,3 +6,23 @@ public class BlowUp {
   public static String blowup(String str) {
     //Create a stringbuilder that will represent a sequence of characters
     StringBuilder userLineOfString = new StringBuilder();
+    
+    
+    //Have the counter represent the number of characters
+    //The counter is at -1 so it will start at the first character
+    int counter = -1;
+
+    //Start a for loop by making the characters in the string into a array
+    for (char character: str.toCharArray()) {
+      //If the counter reads a digit as a character it will go to the next line
+      if (counter < 0 && Character.isDigit(character)) {
+        //this method returns the numeric value of the character, as a non-negative int value.
+        counter = Character.getNumericValue(character);
+      } else {
+        //Creating a do while statement
+        do {
+          userLineOfString.append(character);
+          //The decreement statement must be equal to the value of 0
+        } while (--counter >= 0);
+      }
+    }
