4e3c9f139b199735611e0190d0a22038bde8da1a
TheShiferawShow <68077955+TheShiferawShow@users.noreply.github.com>
2020-12-07 21:00:26 -0500

    Update BlowUp.java

--- BlowUp.java
+++ BlowUp.java
@@ -1,2 +1,8 @@
 //Scan all primitive data types
 import java.util.Scanner;
+
+public class BlowUp {
+  
+  public static String blowup(String str) {
+    //Create a stringbuilder that will represent a sequence of characters
+    StringBuilder userLineOfString = new StringBuilder();
